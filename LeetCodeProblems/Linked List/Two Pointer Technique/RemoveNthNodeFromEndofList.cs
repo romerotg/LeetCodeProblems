29807fd3f6949a349ac078d6fffd1066140b0dbc
romerotg <romerotg@gmail.com>
2019-02-08 09:43:54 -0300

    QUestão removenth node linked list

---
+++ LeetCodeProblems/Linked List/Two Pointer Technique/RemoveNthNodeFromEndofList.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeProblems.Linked_List.Two_Pointer_Technique
+{
+	public class RemoveNthNodeFromEndofList
+	{
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int x) { val = x; }
+		}
+
+		public ListNode RemoveNthFromEnd(ListNode head, int n)
+		{
+			if (head == null)
+				return null;
+
+			ListNode node = head;
+			ListNode beforeRemoved = null;
+			int index = 0;
+
+			while (node != null)
+			{
+				if (index - n == 0)
+					beforeRemoved = head;
+				else if (index - n > 0)
+					beforeRemoved = beforeRemoved.next;
+
+				node = node.next;
+				index++;
+			}
+
+			if (beforeRemoved != null)
+				beforeRemoved.next = beforeRemoved.next.next;
+			else
+				head = head.next;
+
+			return head;
+		}
+	}
+}
---
+++ UnitTestProject1/Linked List/Two Pointer Technique/TestRemoveNthNodeFromEndofList.cs
@@ -0,0 +1,50 @@
+﻿using LeetCodeProblems.Linked_List.Two_Pointer_Technique;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using static LeetCodeProblems.Linked_List.Two_Pointer_Technique.RemoveNthNodeFromEndofList;
+
+namespace UnitTestProject1.Linked_List.Two_Pointer_Technique
+{
+	[TestClass]
+	public class TestRemoveNthNodeFromEndofList
+	{
+		[DataRow(new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 1, 2, 3, 5 })]
+		[DataRow(new int[] { 1 }, 1, null)]
+		[DataRow(new int[] { 1, 2 }, 2, new int[] { 2 })]
+		[DataRow(new int[] { 1, 2 }, 1, new int[] { 1 })]
+		[DataTestMethod]
+		public void TestMethod1(int[] head, int n, int[] expected)
+		{
+			// Arrange
+			RemoveNthNodeFromEndofList question = new RemoveNthNodeFromEndofList();
+			ListNode headNode = null;
+			if (head != null && head.Length > 0)
+			{
+				headNode = new ListNode(head[0]);
+				ListNode node = headNode;
+
+				for (int i = 1; i < head.Length; i++)
+				{
+					node.next = new ListNode(head[i]);
+					node = node.next;
+				}
+			}
+
+			// Act
+			ListNode actual = question.RemoveNthFromEnd(headNode, n);
+			List<int> actualList = null;
+			if (actual != null)
+			{
+				actualList = new List<int>();
+				while (actual != null)
+				{
+					actualList.Add(actual.val);
+					actual = actual.next;
+				}
+			}
+
+			// Assert
+			CollectionAssert.AreEqual(expected, actualList?.ToArray());
+		}
+	}
+}
