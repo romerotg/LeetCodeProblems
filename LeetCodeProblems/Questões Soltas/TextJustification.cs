39df67e8e56056a776307d5a910b555b23c93261
Romero Gonçalves <romerotg@gmail.com>
2019-02-14 08:39:39 -0300

    Questão Text Justification

---
+++ LeetCodeProblems/Questões Soltas/TextJustification.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Questões_Soltas
+{
+	public class TextJustification
+	{
+		public IList<string> FullJustify(string[] words, int maxWidth)
+		{
+			List<string> result = new List<string>();
+			int index = 0;
+			int currentLength = 0;
+
+			while (index < words.Length)
+			{
+				for (int i = index; i < words.Length; i++)
+				{
+					if (currentLength + words[i].Length > maxWidth)
+					{
+						string[] justifiedWords = new string[i - index];
+						Array.Copy(words, index, justifiedWords, 0, justifiedWords.Length);
+						index = i;
+						result.Add(_Justify(justifiedWords, maxWidth, currentLength - 1));
+						currentLength = 0;
+						break;
+					}
+
+					currentLength += words[i].Length + 1;
+
+					if (i == words.Length - 1)
+					{
+						string[] justifiedWords = new string[i - index + 1];
+						Array.Copy(words, index, justifiedWords, 0, justifiedWords.Length);
+						index = i + 1;
+						result.Add(_LeftJustify(justifiedWords, maxWidth));
+						currentLength = 0;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private string _Justify(string[] words, int maxWidth, int currentLength)
+		{
+			if (words.Length == 1)
+				return _LeftJustify(words, maxWidth);
+
+			StringBuilder justifiedText = new StringBuilder();
+
+			int spacesToAddText = maxWidth - currentLength;
+			int spacesToAddWord = spacesToAddText / (words.Length - 1);
+			int extraSpaces = spacesToAddText % (words.Length - 1);
+
+			for (int i = 0; i < words.Length - 1; i++)
+			{
+				justifiedText.Append(words[i]);
+				justifiedText.Append(" ");
+
+				for (int j = 0; j < spacesToAddWord; j++)
+					justifiedText.Append(" ");
+
+				if (extraSpaces-- > 0)
+					justifiedText.Append(" ");
+			}
+
+			justifiedText.Append(words[words.Length - 1]);
+
+			return justifiedText.ToString();
+		}
+
+		private string _LeftJustify(string[] words, int maxWidth)
+		{
+			StringBuilder justifiedText = new StringBuilder();
+			justifiedText.Append(words[0]);
+
+			for (int i = 1; i < words.Length; i++)
+			{
+				justifiedText.Append(" ");
+				justifiedText.Append(words[i]);
+			}
+
+			while (justifiedText.Length < maxWidth)
+				justifiedText.Append(" ");
+
+			return justifiedText.ToString();
+		}
+	}
+}
---
+++ UnitTestProject1/Questões Soltas/TestTextJustification.cs
@@ -0,0 +1,53 @@
+﻿using LeetCodeProblems.Questões_Soltas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject1.Questões_Soltas
+{
+	[TestClass]
+	public class TestTextJustification
+	{
+		[DataRow(new string[] { "This", "is", "an", "example", "of", "text", "justification." }, 16, new string[] { "This    is    an", "example  of text", "justification.  " })]
+		[DataTestMethod]
+		public void TestMethod1(string[] words, int maxWidth, string[] expected)
+		{
+			// Arrange
+			TextJustification question = new TextJustification();
+
+			// Act
+			IList<string> actual = question.FullJustify(words, maxWidth);
+
+			// Assert
+			CollectionAssert.AreEqual(expected, actual?.ToArray());
+		}
+
+		[DataRow(new string[] { "What", "must", "be", "acknowledgment", "shall", "be" }, 16, new string[] { "What   must   be", "acknowledgment  ", "shall be        " })]
+		[DataTestMethod]
+		public void TestMethod2(string[] words, int maxWidth, string[] expected)
+		{
+			// Arrange
+			TextJustification question = new TextJustification();
+
+			// Act
+			IList<string> actual = question.FullJustify(words, maxWidth);
+
+			// Assert
+			CollectionAssert.AreEqual(expected, actual?.ToArray());
+		}
+
+		[DataRow(new string[] { "Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do" }, 20, new string[] { "Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  " })]
+		[DataTestMethod]
+		public void TestMethod3(string[] words, int maxWidth, string[] expected)
+		{
+			// Arrange
+			TextJustification question = new TextJustification();
+
+			// Act
+			IList<string> actual = question.FullJustify(words, maxWidth);
+
+			// Assert
+			CollectionAssert.AreEqual(expected, actual?.ToArray());
+		}
+	}
+}
