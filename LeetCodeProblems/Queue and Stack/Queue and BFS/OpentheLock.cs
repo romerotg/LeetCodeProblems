0d7f24e9364a815108971cb957b5fc554e8d84d7
romerotg <romerotg@gmail.com>
2019-02-19 09:25:19 -0300

    Open the lock queue BFS

---
+++ LeetCodeProblems/Queue and Stack/Queue and BFS/OpentheLock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Queue_and_Stack.Queue_and_BFS
+{
+	public class OpentheLock
+	{
+		public int OpenLock(string[] deadends, string target)
+		{
+			HashSet<string> deadendsHash = new HashSet<string>(deadends);
+
+			if (deadendsHash.Contains("0000"))
+				return -1;
+
+			Queue<string> queue = new Queue<string>();
+			queue.Enqueue("0000");
+			deadendsHash.Add("0000");
+			int turns = 0;
+
+			while (queue.Count > 0)
+			{
+				int size = queue.Count;
+
+				for (int i = 0; i < size; i++)
+				{
+					string current = queue.Dequeue();
+
+					if (current == target)
+						return turns;
+
+					StringBuilder sb = new StringBuilder();
+					string next = null;
+
+					for (int j = 0; j < current.Length; j++)
+					{
+						char c = current[j];
+						sb.Clear();
+						sb.Append(current);
+						sb[j] = c == '9' ? '0' : (char)(c + 1);
+						next = sb.ToString();
+						if (!deadendsHash.Contains(next))
+						{
+							deadendsHash.Add(next);
+							queue.Enqueue(next);
+						}
+						sb.Clear();
+						sb.Append(current);
+						sb[j] = c == '0' ? '9' : (char)(c - 1);
+						next = sb.ToString();
+						if (!deadendsHash.Contains(next))
+						{
+							deadendsHash.Add(next);
+							queue.Enqueue(next);
+						}
+					}
+				}
+
+				turns++;
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ UnitTestProject1/Queue and Stack/Queue and BFS/TestOpentheLock.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeProblems.Queue_and_Stack.Queue_and_BFS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.Queue_and_Stack.Queue_and_BFS
+{
+	[TestClass]
+	public class TestOpentheLock
+	{
+		[DataRow(new string[] { "0201", "0101", "0102", "1212", "2002" }, "0202", 6)]
+		[DataRow(new string[] { "8888" }, "0009", 1)]
+		[DataRow(new string[] { "8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888" }, "8888", -1)]
+		[DataRow(new string[] { "0000" }, "8888", -1)]
+		[DataTestMethod]
+		public void TestMethod1(string[] deadends, string target, int expected)
+		{
+			// Arrange
+			OpentheLock question = new OpentheLock();
+
+			// Act
+			int actual = question.OpenLock(deadends, target);
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
