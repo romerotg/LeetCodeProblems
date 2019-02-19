7e2127b0dd8208fa6eae82096e21d1d4151ac23b
Romero Gonçalves <romerotg@gmail.com>
2019-02-19 11:02:15 -0300

    Queue BFS - perfect squares

---
+++ LeetCodeProblems/Queue and Stack/Queue and BFS/PerfectSquares.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeProblems.Queue_and_Stack.Queue_and_BFS
+{
+	public class PerfectSquares
+	{
+		public int NumSquares(int n)
+		{
+			if (n <= 0)
+				return 0;
+
+			List<int> perfectSquares = _GetPossiblePerfectSquares(n);
+
+			Queue<int> queue = new Queue<int>();
+			queue.Enqueue(0);
+			int level = 0;
+
+			while (queue.Count > 0)
+			{
+				int size = queue.Count;
+
+				for (int i = 0; i < size; i++)
+				{
+					int sum = queue.Dequeue();
+
+					if (sum == n)
+						return level;
+
+					for (int j = perfectSquares.Count - 1; j >= 0; j--)
+					{
+						int newSum = sum + perfectSquares[j];
+						if (newSum <= n)
+							queue.Enqueue(newSum);
+					}
+				}
+
+				level++;
+			}
+
+			return 0;
+		}
+
+		private List<int> _GetPossiblePerfectSquares(int n)
+		{
+			List<int> result = new List<int>();
+			int i = 1;
+			int power = 0;
+
+			while (power < n)
+			{
+				power = i * i;
+				if (power <= n)
+					result.Add(power);
+				i++;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ UnitTestProject1/Queue and Stack/Queue and BFS/TestPerfectSquares.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems.Queue_and_Stack.Queue_and_BFS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1.Queue_and_Stack.Queue_and_BFS
+{
+	[TestClass]
+	public class TestPerfectSquares
+	{
+		[DataRow(12, 3)]
+		[DataRow(13, 2)]
+		[DataRow(7168, 4)]
+		[DataTestMethod]
+		public void TestMethod1(int n, int expected)
+		{
+			// Arrange
+			PerfectSquares question = new PerfectSquares();
+
+			// Act
+			int actual = question.NumSquares(n);
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
