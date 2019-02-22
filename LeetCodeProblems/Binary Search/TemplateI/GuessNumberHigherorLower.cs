cfbd780d3a34bc8cf1e5b2c9f717e57e8946fb3b
Romero Gonçalves <romerotg@gmail.com>
2019-02-22 11:09:41 -0300

    Remoção imports

--- LeetCodeProblems/Binary Search/TemplateI/GuessNumberHigherorLower.cs
+++ LeetCodeProblems/Binary Search/TemplateI/GuessNumberHigherorLower.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeProblems.Binary_Search.TemplateI
+﻿namespace LeetCodeProblems.Binary_Search.TemplateI
 {
 	public class GuessNumberHigherorLower
 	{
