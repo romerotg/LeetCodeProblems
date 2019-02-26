9f0a376068af73c0d3e5c3f72354400069cebc0a
romerotg <romerotg@gmail.com>
2019-02-26 08:55:15 -0300

    Finalização questões template I binary search

---
+++ LeetCodeProblems/Binary Search/TemplateI/SearchinRotatedSortedArray.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeProblems.Binary_Search.TemplateI
+{
+	public class SearchinRotatedSortedArray
+	{
+		public int Search(int[] nums, int target)
+		{
+			if (nums == null || nums.Length == 0)
+				return -1;
+
+			int left = 0;
+			int right = nums.Length - 1;
+
+			while (left <= right)
+			{
+				int middle = left + (right - left) / 2;
+
+				if (nums[middle] == target)
+					return middle;
+
+				// right ordered
+				if (nums[middle] < nums[right])
+				{
+					if (nums[middle] < target && nums[right] >= target)
+						left = middle + 1;
+					else
+						right = middle - 1;
+
+				}
+				// left part ordered
+				else
+				{
+					if (nums[middle] > target && nums[left] <= target)
+						right = middle - 1;
+					else
+						left = middle + 1;
+				}
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ UnitTestProject1/Binary Search/TemplateI/TestSearchinRotatedSortedArray.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeProblems.Binary_Search.TemplateI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Binary_Search.TemplateI
+{
+	[TestClass]
+	public class TestSearchinRotatedSortedArray
+	{
+		[DataRow(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+		[DataRow(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+		[DataRow(new int[] { 5, 1, 3 }, 5, 0)]
+		[DataRow(new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 8, 4)]
+		[DataRow(new int[] { 3, 4, 5, 6, 7, 8, 1, 2 }, 7, 4)]
+		[DataTestMethod]
+		public void TestMethod1(int[] nums, int target, int expected)
+		{
+			// Arrange
+			SearchinRotatedSortedArray question = new SearchinRotatedSortedArray();
+
+			// Act
+			int actual = question.Search(nums, target);
+
+			// Result
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
