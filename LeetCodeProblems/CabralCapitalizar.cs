358964736e08b4177ebef664f701937093ba5181
romerotg <romerotg@gmail.com>
2019-04-03 08:46:37 -0300

    Questão capitaliza kbral

---
+++ LeetCodeProblems/CabralCapitalizar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+	public class CabralCapitalizar
+	{
+		public string CapitalizeString(string s)
+		{
+			if (s == null || s.Length == 0)
+				return s;
+
+			StringBuilder sb = new StringBuilder();
+			sb.Append(char.ToUpper(s[0]));
+
+			bool shouldAddSpace = false;
+			bool shouldCapitalize = false;
+
+			for (int i = 1; i < s.Length; i++)
+			{
+				char c = s[i];
+				if (c == ' ')
+					shouldAddSpace = false;
+				else
+				{
+					bool currShouldAddSpace = _ShouldAddSpace(c);
+					if (shouldAddSpace && !currShouldAddSpace)
+						sb.Append(' ');
+
+					bool currShouldCapitalize = _ShouldCapitalize(c);
+					if (shouldCapitalize && !currShouldCapitalize)
+						c = char.ToUpper(c);
+
+					shouldAddSpace = currShouldAddSpace;
+					shouldCapitalize = currShouldCapitalize;
+				}
+
+				sb.Append(c);
+			}
+
+			return sb.ToString();
+		}
+
+		private bool _ShouldAddSpace(char c)
+		{
+			return c == '.' || c == ',' || c == ';' || c == '?' || c == '!';
+		}
+
+		private bool _ShouldCapitalize(char c)
+		{
+			return c == '.' || c == '?' || c == '!';
+		}
+	}
+}
---
+++ UnitTestProject1/TestCabralCapitalizar.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class TestCabralCapitalizar
+	{
+		[DataRow("the conference has people who have come from Moscow,Idaho;Paris,Texas;London,Ohio; and other places as well.", "The conference has people who have come from Moscow, Idaho; Paris, Texas; London, Ohio; and other places as well.")]
+		[DataRow("oi...tudo bom?", "Oi... Tudo bom?")]
+		[DataRow("mas o que é isso!?você está louco?!", "Mas o que é isso!? Você está louco?!")]
+		[DataTestMethod]
+		public void TestMethod1(string s, string expected)
+		{
+			// Arrange
+			CabralCapitalizar question = new CabralCapitalizar();
+
+			// Act
+			string actual = question.CapitalizeString(s);
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
